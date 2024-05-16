dd4d156158c58d82cd94876d397965b3b5b3937a
Anthony Clavio <aclavio@confluent.io>
2024-05-16 17:13:28 -0400

    removed dependencies, initial consumption

--- person-builder-app/src/main/java/io/confluent/demo/PersonBuilder.java
+++ person-builder-app/src/main/java/io/confluent/demo/PersonBuilder.java
@@ -1,8 +1,104 @@
 package io.confluent.demo;
 
-public class PersonBuilder {
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    public static void main(String ...args) {
-        
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class PersonBuilder implements Runnable {
+
+    private static final Logger logger = LoggerFactory.getLogger(PersonBuilder.class);
+
+    private static final String DEFAULT_CONFIG_FILE = "streams.properties";
+
+    private Properties config;
+    private final KafkaStreams streams;
+
+    public PersonBuilder(final Properties config) {
+        setConfig(config);
+        streams = new KafkaStreams(getTopology(), config);
+    }
+
+    public void setConfig(final Properties config) {
+        if (!config.containsKey("person.raw.topic"))
+            throw new RuntimeException("Missing required property ".concat("person.raw.topic"));
+        if (!config.containsKey("person.enriched.topic"))
+            throw new RuntimeException("Missing required property ".concat("person.enriched.topic"));
+        this.config = config;
+    }
+
+    private Topology getTopology() {
+        final String INPUT_TOPIC = config.getProperty("person.raw.topic");
+        final String OUTPUT_TOPIC = config.getProperty("person.enriched.topic");
+
+        // configure Serdes
+        final Serde<String> stringSerde = Serdes.String();
+        final GenericAvroSerde genericAvroSerde = new GenericAvroSerde();
+        genericAvroSerde.configure((Map)config, false);
+        final SpecificAvroSerde<Person> personAvroSerde = new SpecificAvroSerde<>();
+        personAvroSerde.configure((Map)config, false);
+
+        // build the streaming topology
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        // TODO app logic
+        builder
+                .stream(INPUT_TOPIC, Consumed.with(stringSerde, genericAvroSerde))
+                .peek((key, genericRecord) -> logger.info("got record: [{}]{}", key, genericRecord.toString()));
+
+        return builder.build();
+    }
+
+    @Override
+    public void run() {
+        final CountDownLatch latch = new CountDownLatch(1);
+        streams.setStateListener((newState, oldState) -> {
+            logger.info("state change: {} -> {}", oldState, newState);
+            if ((oldState == KafkaStreams.State.RUNNING ||
+                    oldState == KafkaStreams.State.REBALANCING )
+                    && newState != KafkaStreams.State.RUNNING) {
+                logger.info("latch decrementing");
+                latch.countDown();
+            }
+        });
+
+        // Use this in development to clean the local streams state
+        streams.cleanUp();
+        // start the streams app
+        streams.start();
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            logger.debug("PersonBuilder closed!");
+        }
+    }
+
+    public void stop() {
+        streams.close();
+    }
+
+    public static void main(String ...args) throws IOException {
+        String configPath = args.length > 0 ? args[0] : DEFAULT_CONFIG_FILE;
+        final Properties cfg = new Properties();
+        cfg.load(new FileInputStream(configPath));
+
+        final PersonBuilder personBuilder = new PersonBuilder(cfg);
+        Runtime.getRuntime().addShutdownHook(new Thread(personBuilder::stop));
+        personBuilder.run();
     }
 }
